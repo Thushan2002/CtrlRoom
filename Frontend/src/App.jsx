4899a1891283fc05a4d7e74d45d99559a0fe2ff3
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-08-08 13:56:23 +0530

    feat: home page UI improved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <HomePage />
-    </>
+    <div className=" bg-background-2">
+      <div className="w-[95%] sm:w-[90%] md:w-[90%] mx-auto pt-6 md:pt-10">
+        <Navbar />
+        <HomePage />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
