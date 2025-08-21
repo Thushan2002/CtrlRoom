5a9e9249301852b622e16731c9977da8f37c9151
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-21 11:38:52 +0530

    signup is added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
 import Footer from "./Components/Footer";
+import SignUp from "./Pages/Signup";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className=" bg-background-2">
       <div className="w-[95%] sm:w-[90%] md:w-[90%] mx-auto pt-6 md:pt-10">
         <Navbar />
-        <HomePage />
+       <Routes>
+        <Route path = '/' element = {<HomePage/>} />
+        <Route path = '/signup' element = {<SignUp/>} />
+       </Routes>
       </div>
       <Footer />
     </div>
