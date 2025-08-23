c9f2ab64e2a8c2943c71cf97490e0a646f1e16c4
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-08-23 12:35:53 +0530

    Developed & added Login Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,26 @@ import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
 import Footer from "./Components/Footer";
 import SignUp from "./Pages/Signup";
-import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
+  const location = useLocation();
+
+  const hideNavbarPaths = ["/login"];
+  const shouldShowNavbar = !hideNavbarPaths.includes(location.pathname);
+
   return (
-    <div className=" bg-background-2">
+    <div className="bg-background-2">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <div className="w-[95%] sm:w-[90%] md:w-[90%] mx-auto pt-6 md:pt-10">
-        <Navbar />
-       <Routes>
-        <Route path = '/' element = {<HomePage/>} />
-        <Route path = '/signup' element = {<SignUp/>} />
-       </Routes>
+        {shouldShowNavbar && <Navbar />}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </div>
       <Footer />
     </div>
---
+++ Frontend/src/Pages/Login.jsx
@@ -0,0 +1,78 @@
+import Logo from "../assets/images/logo.png";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+
+function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  if (isLoggedIn) {
+    return <Navigate to="/signup" />;
+  }
+
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col bg-center relative">
+        <div className="flex bg-blue-900 justify-between p-4 sm:p-6 z-10">
+          <div className="flex items-center mx-4 sm:mx-8">
+            <img 
+              src={Logo} 
+              alt="Ctrl Room Logo" 
+              className="h-10 w-12 sm:h-12 sm:w-14"
+            />
+            <h1 className="text-lg sm:text-xl text-white font-semibold mx-2 py-2">
+              Ctrl Room
+            </h1>
+          </div>
+          <nav className="flex items-center gap-4">
+            <button className="bg-white rounded-2xl text-indigo-900 text-sm px-4 py-2 sm:w-25 sm:h-10 
+              hover:shadow-xl cursor-pointer border-2 border-indigo-950 font-bold" onClick={() => {setIsLoggedIn(true)}}>
+              Sign Up
+            </button>
+          </nav>
+        </div>
+        <div className="flex flex-col items-center justify-center flex-grow w-full px-4">
+          <div className="w-full max-w-md sm:max-w-lg flex flex-col justify-center text-center">
+            <h1 className="text-2xl sm:text-3xl font-bold text-blue-800">
+              Welcome to
+            </h1>
+            <h2 className="text-3xl sm:text-5xl font-bold px-4 sm:px-15">
+              <span className="text-blue-950">Ctrl</span>
+              <span className="text-blue-600">Room</span>
+            </h2>
+            <p className="font-bold text-lg sm:text-xl mt-2">
+              Computer Lab Management System
+            </p>
+          </div>
+          <div className="bg-blue-100 rounded-xl text-center w-[400px] h-[300px] flex flex-col 
+          items-center relative z-10 mt-10">
+            <h1 className="text-3xl font-bold text-blue-800 mt-5">Sign In</h1>
+            <input
+              type="text"
+              id="uname"
+              name="uname"
+              placeholder="Username"
+              className="border-2 border-gray-300 pl-3 rounded-xl mt-5 w-80 h-12"
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className="border-2 border-gray-300 pl-3 rounded-xl mt-3 w-80 h-12"
+            />
+            <a href="#" className="text-xs font-bold text-blue-950 mt-2 block
+            ">Forgot Password?</a>
+            <button className="bg-blue-900 rounded-xl w-80 h-10 text-white font-bold mt-3 cursor-pointer">
+              Sign In
+            </button>
+            <p className="text-xs mt-2 font-bold">
+              Don't have an account? <a href="#" className="text-blue-900 pl-3">Sign Up</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
